fabc3901f8ae37bdc063734fc41156603394226f
Enis AKGÜN <enisakgun@ogr.eskisehir.edu.tr>
2023-10-09 15:23:09 +0300

    saat eklendi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void Main (String [] args){
-        System.out.println("Github public repository deneme Enis 09.10.2023 ");
+        System.out.println("Github public repository deneme Enis 09.10.2023  15.22");
     }
 }
