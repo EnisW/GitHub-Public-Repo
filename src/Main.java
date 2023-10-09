61f1ce72531cbec80a435ba72aaccdc40c0a6f8c
Enis AKGÜN <enisakgun@ogr.eskisehir.edu.tr>
2023-10-09 15:34:40 +0300

    artik calisiyo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void Main (String [] args){
+    public static void main(String [] args){
         System.out.println("Github public repository deneme Enis 09.10.2023  15.22");
     }
 }
